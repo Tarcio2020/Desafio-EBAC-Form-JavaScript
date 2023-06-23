5a50e9c082764d8c4723608f87423e86655ebeb1
Tarcio Teles <tarcioapps7@gmail.com>
2023-06-23 00:45:40 -0300

    add

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+form.addEventListener('submit', function(e) {
+
+    let um = document.getElementById('#a');
+    let dois = document.getElementById('#b');
+    
+    if(um>dois){
+        alert("sucess")
+    }
+    else {
+
+      alert("not")
+
+    }
+})
+
+
