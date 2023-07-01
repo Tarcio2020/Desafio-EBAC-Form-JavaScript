666c60c85141a18f7b41dfb1ea5bfd289a47b040
Tarcio Teles <tarcioapps7@gmail.com>
2023-06-30 23:55:33 -0300

    add

--- validation.js
+++ validation.js
@@ -5,15 +5,32 @@ formulario.addEventListener('submit', function(e){
     e.preventDefault()
     validation()
 
-    async function validation() {
+    function validation() {
         const valorMaior = document.getElementById('campA').value;
         const valorMenor = document.getElementById('campB').value;
+        const error = document.getElementById('p1').style.display = 'block';
+
+        function errorNumber() {
+            console.log(error)
+        }
+
+        function backFormClean() {
+            valorMaior.value = ' ';
+            valorMenor.value = ' ';
+        }
     
         if (valorMenor > valorMaior) {
-            alert('Campo B maior que o Campo A');
+            errorNumber()
+            backFormClean() 
+           
+
         } else {
             alert('Campo B menor que o Campo A');
         }
+
+        
+
+
     }
 
 })
